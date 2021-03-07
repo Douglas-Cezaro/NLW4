4a882c70f42946a7122ef9b31fb739aebea1d544
douglas-cezaro <cezarodouglas@gmail.com>
2021-03-07 13:33:15 -0300

    Third Class

--- moveit/src/pages/index.tsx
+++ moveit/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -18,7 +19,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
